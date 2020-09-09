25dcfe11ffbff874eec3e4455f1803ecc6e184ea
max-ans <maxenceroyer@hotmail.com>
2020-09-09 16:30:03 +0200

    Provider in index.js / react-redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from 'src/store';
 
 import App from 'src/components/App';
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
