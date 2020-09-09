97023ff9800ae41300efe4b77976517ab643df1c
max-ans <maxenceroyer@hotmail.com>
2020-09-09 16:52:54 +0200

    useSelector in NbColors component

--- src/components/NbColors/index.js
+++ src/components/NbColors/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+// useSelector est un hook qui permet de lire une
+// information dans le store Redux
+import { useSelector } from 'react-redux';
 
-const NbColors = () => (
-  <div id="nbColors">
-    5 couleur(s) générée(s)
-  </div>
-);
-
+const NbColors = () => {
+  const nbColors = useSelector((state) => state.nbColors);
+  return (
+    <div id="nbColors">
+      {nbColors} couleur(s) générée(s)
+    </div>
+  );
+};
 export default NbColors;
